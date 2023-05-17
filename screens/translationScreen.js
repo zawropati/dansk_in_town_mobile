dc30974465ba44078c9598eaa47f4990ed0e84cb
zawropati <zawrotniakpatrycja@gmail.com>
2023-05-17 09:53:35 +0200

    daily streak, notification

--- screens/translationScreen.js
+++ screens/translationScreen.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getTranslationsForExercises, tooEasy, addToFavourites, checkIfFavourite, addEvent, getOptionsAnswers } from "../calls/db.js";
+import { getTranslationsForExercises, checkIfFavourite, addEvent, getOptionsAnswers, getStrike, handleStrike } from "../calls/db.js";
 // import { Link } from "react-router-dom";
 import {
-    StyleSheet, ScrollView, View, Text, Image, TouchableOpacity, ActivityIndicator,
+    StyleSheet, ScrollView, View, Text, Image, TouchableOpacity, ActivityIndicator, Modal
   } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { User } from 'parse/react-native.js';
 import { useRoute } from '@react-navigation/native';
 import {useWindowDimensions} from 'react-native';
 
@@ -16,7 +15,6 @@ const TranslationScreen = () => {
     const [wrongs, setWrongs] = useState();
     const [state, setIndex] = useState(-1);
     const [word, setWord] = useState();
-    const [currentScore, setScore] = useState(0);
     const [practiceFavourites, setPractice] = useState(false)
     const [transData, setData] = useState()
     const [loading, setLoading] = useState(false)
@@ -24,6 +22,10 @@ const TranslationScreen = () => {
     const [color, setColor] = useState('#40F99B')
     const [correct, setCorrect ] = useState()
     const [loadingImage, setLoadedImage] = useState(false);
+    const [days, setDays] = useState();
+    const [dailyCorrect, setDailyCorrect] = useState(0);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
 
     const route = useRoute();
 
@@ -36,8 +38,20 @@ const TranslationScreen = () => {
       }else{
         getRandomTranslation();
       }
+      checkStrike()
     }, [route.params]);
 
+    const checkStrike = function(){
+      getStrike().then((result)=>{
+        if(result !== 0){
+          setDailyCorrect(result.correctAnswersCount)
+          setDays(result.strikeDays)
+        }else{
+          setDays(result)
+          setDailyCorrect(0)
+        }
+      })
+    }
     // useEffect(() => {
     //   getRandomTranslation();
     // }, [practiceFavourites]);
@@ -63,7 +77,6 @@ const TranslationScreen = () => {
               setLoading(false)
             })
           }else{
-            console.log('else')
             getOptionsAnswers(correctWord).then((results) => {
               let imageId = correctWord.get("image").id
               let userId = Parse.User.current().id
@@ -82,18 +95,17 @@ const TranslationScreen = () => {
         });
       }
 
-      function handleFavourties(data) {
-        const correctWord = data[Math.floor(Math.random() * data.length)];
+    function handleFavourties(data) {
+      const correctWord = data[Math.floor(Math.random() * data.length)];
+        getOptionsAnswers(correctWord).then((results) => {
+          results.push(correctWord.get("to").toLowerCase())
+          shuffleArray(results)
+          setWrongs(results)
+          setTranslation(correctWord)
+          setLoading(false)
 
-          getOptionsAnswers(correctWord).then((results) => {
-            results.push(correctWord.get("to").toLowerCase())
-            shuffleArray(results)
-            setWrongs(results)
-            setTranslation(correctWord)
-            setLoading(false)
-
-          })
-      }
+        })
+    }
 
     function shuffleArray (array) {
       for (var i = array.length - 1; i > 0; i--) {
@@ -105,14 +117,6 @@ const TranslationScreen = () => {
       return array;
     }
 
-    const handleTooEasy = function () {
-      tooEasy(translation).then(() => {
-        if(!practiceFavourites){
-          getRandomTranslation();
-        }else{handleFavourties(transData)}
-      });
-    }
-
     const pickTile = function (e, index) {
       setWord(e)
       setIndex(index)
@@ -120,114 +124,123 @@ const TranslationScreen = () => {
         if(e.toLowerCase() == translation.get('to').toLowerCase()){
           // addEvent('correctAnswer', translation.id)
           // setIndex(-1);
+          saveStrike()
           setCorrect(true)
-          setScore(currentScore + 1)
           if(!practiceFavourites){
             //
           }else{handleFavourties(transData)}
           }else{
-              console.log('here')
               // addEvent('wrongAnswer', translation.id)
               // setIndex(-1);
               setCorrect(false)
-
             }
             setTimeout(() => {
               getRandomTranslation()
-            }, 2000);
+            }, 1500);
+        }, 1000);
+
+  }
+  const saveStrike = function (){
+    handleStrike(days).then((strike)=>{
+      setDailyCorrect(strike.get('correctAnswersCount'))
+      if(strike.get('correctAnswersCount') == 10){
+        setTimeout(() => {
+          setIsModalVisible(true);
         }, 2000);
-
-    }
-
-	const addFavourites = function (){
-		let imgId = translation.get('image').id
-    let userId = User.current().id
-    addToFavourites(imgId, userId)
-	}
-
+      }
+      setDays(strike.get('strikeDays'))
+    })
+  }
   const exitFavourites = function (){
     setPractice(false)
     getRandomTranslation(false);
   }
-  if (!translation || loading) {
-    return (
-      <View style={{  backgroundColor: '#FFFDFB', height: '100%', paddingTop: 10}}>
-        <ActivityIndicator  size="large" color="#F06543"/>
-      </View>
-    )
+  const toggleModal = function (){
+    setIsModalVisible(!isModalVisible)
   }
+
   return (
     <View style={{  backgroundColor: '#FFFDFB', height: windowHeight, flex: 1}}>
-      {practiceFavourites && (<TouchableOpacity
+      {/* <TouchableOpacity onPress={toggleModal}><Text>click</Text></TouchableOpacity> */}
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        onRequestClose={toggleModal}
+      >
+        <View style={styles.popupBox}>
+        <Ionicons name={'flame'} size={70} color={'#F06543'}/>
+          <Text style={{...styles.popupText, fontSize: 50, marginBottom:20}}> day {days}</Text>
+          <Text style={styles.popupText}>Congrats!</Text>
+          <Text style={styles.popupTextSmall}>Streak mode activated! Maintain your progress by getting 10 phrases correctly every day. </Text>
+          <TouchableOpacity style={styles.button} onPress={toggleModal}><Text style={styles.popupBtnText}>Close</Text></TouchableOpacity>
+        </View>
+      </Modal>
+      <View style={styles.strikeBox}>
+        <View style={styles.strikeBoxOne}><Text  style={styles.strikeText}>daily streak</Text><Ionicons name={'flame'} size={25} color={'#F06543'}></Ionicons><Text style={{ ...styles.strikeText, fontSize: 20}}>{days}</Text></View>
+        <View style={styles.strikeBoxOne}><Text  style={styles.strikeText}>correct today</Text><Ionicons name={'checkmark-circle'} size={25} color={'#2ae284'}></Ionicons><Text style={{ ...styles.strikeText, fontSize: 20}}>{dailyCorrect}</Text></View>
+      </View>
+    { (!loading && translation) ? (
+      <><View style={{
+          alignItems: 'center', height: '55%', width: '100%', paddingTop: 10,
+          paddingBottom: 10,
+          borderBottomWidth: 2,
+          borderStyle: 'solid',
+          borderBottomColor: '#F06543',
+        }}>
+          <Image alt=""
+            style={{ width: '100%', height: '100%', resizeMode: 'contain' }}
+            src={translation.get("image").get("file").url()}
+            onLoad={() => setLoadedImage(true)} />
+                  {practiceFavourites && (<TouchableOpacity
         style={{
           backgroundColor: '#ffffff', alignSelf: 'center',
           borderRadius: 50, width: 180, display: 'flex',
           flexDirection: 'row', padding: 1, position: 'absolute',
-          zIndex:100, marginTop: 10, justifyContent: 'center',
+          zIndex:100, marginTop: 15, justifyContent: 'center',
         }}
           onPress={() => exitFavourites()} variant="secondary"
         >
         <Ionicons size={25} color='#F06543' name='close-outline'/>
         <Text style={{fontSize: 16, paddingTop: 3}}>Exit library mode</Text>
       </TouchableOpacity>)}
-    <View style={{alignItems: 'center', height: '45%', width: '100%',paddingTop: 10,
-      paddingBottom: 10,
-      borderBottomWidth: 2,
-      borderStyle: 'solid',
-      borderBottomColor: '#F06543',
-      }}>
-        <Image alt=""
-        style={{width: '100%', height: '100%', resizeMode: 'contain'}}
-        src={translation.get("image").get("file").url()}
-        onLoad={() => setLoadedImage(true)}
-        />
-      {/* <ImageBackground style={{width: 350, height: 300, resizeMode: 'contain', flex: 1}} source={require('../assets/graph(4).png')} resizeMode="cover"> */}
-        {/* //  style={{resizeMode: 'contain', width:'75%', height:'75%', alignSelf: 'center', justifyContent:'center', marginTop: 20}} */}
-        {/* </ImageBackground> */}
-        <View style={{display: 'flex', padding: 10, flexDirection: 'row', textAlign: 'center'}}>
-          {/* {isFavourite && !practiceFavourites && (<TouchableOpacity
-          style={{
-            backgroundColor: '#ffffff', borderRadius: 50, width: 160, display: 'flex', flexDirection: 'row', padding: 1}}
-            onPress={() => addFavourites()} variant="secondary"
-          >
-            <Ionicons size={25} color='#F06543' name='add-outline'/>
-            <Text style={{fontSize: 16, paddingTop: 3}}>Add to favourites</Text>
-          </TouchableOpacity>)} */}
+        </View><ScrollView style={{ marginTop: 20 }}>
+            <Text style={{ textAlign: 'center', fontSize: 22, fontFamily: 'Archivo_Black', fontWeight: 200, marginBottom: 5 }}>
+              {translation.get("from")}
+            </Text>
+            <View style={styles.wordContainer}>
+              {wrongs.map((e, index) => (
+                <TouchableOpacity key={index}
+                  style={{
+                    backgroundColor: state === index && correct ? "#2ae284" : state === index && correct === false ? "red" : "#F3F3FF",
+                    borderWidth: state === index ? 2 : 0,
+                    margin: 5,
+                    paddingBottom: 25,
+                    paddingTop: 25,
+                    paddingLeft: 10,
+                    paddingRight: 10,
+                    // flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '45%',
+                    textAlign: 'center',
+                    borderRadius: 5,
+                    transition: '1s ease-in-out',
+                  }}
+                  onPress={() => pickTile(e, index)} variant="secondary"
+                >
+                  <Text style={{ textAlign: 'center', fontSize: 18, fontFamily: 'Archivo', fontWeight: 200, }}>{e}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView></>
+      ) : (
+        <View style={{  backgroundColor: '#FFFDFB', height: '100%', paddingTop: 10}}>
+        <ActivityIndicator  size="large" color="#F06543"/>
+      </View>
+      )}
         </View>
-      </View>
-      <ScrollView style={{marginTop: 20}}>
-        <Text style={{textAlign: 'center', fontSize: 22, fontFamily: 'Archivo_Black', fontWeight: 200, marginBottom: 5}}>
-          {translation.get("from")}
-        </Text>
-      <View style={styles.wordContainer}>
-          {wrongs.map((e, index) => (
-            <TouchableOpacity key={index}
-            style={{
-              backgroundColor: state === index && correct ? "#40F99B" : state === index && correct === false ? "red" : "#F3F3FF",
-              borderWidth: state === index ? 2 : 0,
-              margin: 5,
-              paddingBottom: 25,
-              paddingTop: 25,
-              paddingLeft: 10,
-              paddingRight: 10,
-              // flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '45%',
-              textAlign: 'center',
-              borderRadius: 5,
-              transition: '1s ease-in-out',
-              }}
-            onPress={() => pickTile(e, index)} variant="secondary"
-          >
-              <Text style={{textAlign: 'center', fontSize: 18, fontFamily: 'Archivo', fontWeight: 200,}}>{e}</Text>
-              </TouchableOpacity>
-          ))}
-      </View>
-    </ScrollView>
-    </View>
-  );
-}
+        );
+      }
 
 const styles = StyleSheet.create({
   wordContainer: {
@@ -245,18 +258,63 @@ const styles = StyleSheet.create({
       backgroundColor: '#FFF8F1',
       fontWeight: 600,
       fontFamily: 'Archivo_Black',
-  },
+    },
   button: {
-      backgroundColor: '#F06543',
-      width: 200,
+      backgroundColor: "white",
       padding: 10,
+      width: 100,
       borderRadius: 20,
-      alignSelf: 'center',
+      alignItems: 'center',
+      margin: 20
   },
   text: {
       textAlign: 'center',
       fontSize: 20,
       color: 'white',
+  },
+  strikeBox:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    backgroundColor: '#FFF8F1',
+    padding: 5
+    // verticalAlign: 'center',
+  },
+  strikeText: {
+    fontFamily: 'Archivo_Black',
+    marginTop: 5,
+    marginBottom: 5,
+    marginRight: 5,
+    fontSize: 12,
+    alignSelf: 'center',
+    justifyContent: 'center', //Centered vertically
+  },
+  popupBox: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#4845ed' },
+  popupText: {
+    fontFamily: 'Archivo_Black',
+    fontSize: 26,
+    color: 'white',
+    padding: 10
+  },
+  popupTextSmall: {
+    textAlign: 'center',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    fontFamily: 'Archivo',
+    fontSize: 20,
+    color: 'white'
+  },
+  popupBtnText: {
+    fontFamily: 'Archivo_Black',
+    fontSize: 14,
+    color: 'black',
+  },
+  strikeBoxOne: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10
   }
 });
 
